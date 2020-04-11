aa395ba935127502a8a4d328f461f5a7c2b7797b
Hasib Ullah <hasib.ascend@gmail.com>
2020-04-12 00:03:18 +0600

    Phone SignIn Authentication Completed

--- daily_task/lib/screens/phoneSignInScreen.dart
+++ daily_task/lib/screens/phoneSignInScreen.dart
@@ -1,4 +1,5 @@
 import 'package:daily_task/config/config.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
@@ -9,6 +10,14 @@ class PhoneSignInScreen extends StatefulWidget {
 
 class _PhoneSignInScreenState extends State<PhoneSignInScreen> {
   PhoneNumber _phoneNumber;
+
+  String _message;
+  String _verificationId;
+  bool _isSMSsent = false;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _smsController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +25,8 @@ class _PhoneSignInScreenState extends State<PhoneSignInScreen> {
         title: Text("Sign in using Phone Number"),
       ),
       body: SingleChildScrollView(
-        child: Container(
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 500),
           margin: EdgeInsets.all(12.0),
           width: MediaQuery.of(context).size.width,
           child: Column(
@@ -26,37 +36,150 @@ class _PhoneSignInScreenState extends State<PhoneSignInScreen> {
                   _phoneNumber = phoneNumbertxt;
                 },
                 inputBorder: OutlineInputBorder(),
-                initialCountry2LetterCode: "BD",
+                initialCountry2LetterCode: "US",
               ),
-              InkWell(
-              onTap: () {
-               // _signin();
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      primaryColor,
-                      secondaryColor,
-                    ],
+
+
+              _isSMSsent ? Container(
+                margin: EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _smsController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "OTP here",
+                    labelText: "OTP",
                   ),
-                  borderRadius: BorderRadius.circular(8.0),
+
+                  maxLength: 6,
+                  keyboardType: TextInputType.number,
                 ),
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                child: Center(
-                    child: Text(
-                  "Send OTP",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                )),
-              ),
-            ),
+              ) : Container(),
+
+              !_isSMSsent
+                  ? InkWell(
+                      onTap: () {
+                        setState(() {
+                          _isSMSsent = true;
+                        });
+                        
+                        _verifyPhoneNumber();
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              primaryColor,
+                              secondaryColor,
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                        child: Center(
+                            child: Text(
+                          "Send OTP",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                    )
+                  : InkWell(
+                      onTap: () {
+                        _signInWithPhoneNumber();
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              primaryColor,
+                              secondaryColor,
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                        child: Center(
+                            child: Text(
+                          "Verify OTP",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                    ),
             ],
           ),
         ),
       ),
     );
   }
+
+  void _verifyPhoneNumber() async {
+    setState(() {
+      _message = '';
+    });
+
+    final PhoneVerificationCompleted verificationCompleted =
+        (AuthCredential phoneAuthCredential) {
+      _auth.signInWithCredential(phoneAuthCredential);
+      setState(() {
+        _message = 'Received phone auth credential: $phoneAuthCredential';
+      });
+    };
+
+    final PhoneVerificationFailed verificationFailed =
+        (AuthException authException) {
+      setState(() {
+        _message =
+            'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}';
+      });
+    };
+
+    final PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
+      _verificationId = verificationId;
+    };
+
+    final PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      _verificationId = verificationId;
+    };
+
+    await _auth.verifyPhoneNumber(
+        phoneNumber: _phoneNumber.phoneNumber,
+        timeout: const Duration(seconds: 120),
+        verificationCompleted: verificationCompleted,
+        verificationFailed: verificationFailed,
+        codeSent: codeSent,
+        codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+  }
+
+  // Example code of how to sign in with phone.
+  void _signInWithPhoneNumber() async {
+    final AuthCredential credential = PhoneAuthProvider.getCredential(
+      verificationId: _verificationId,
+      smsCode: _smsController.text,
+    );
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        _message = 'Successfully signed in, uid: ' + user.uid;
+        print(_message);
+      } else {
+        _message = 'Sign in failed';
+      }
+    });
+
+  }
+
 }
